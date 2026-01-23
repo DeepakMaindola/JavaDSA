3ef42ae73feb9b774323420c3177a39ca7cf59be
Deepak Maindola <deepakmaindola.linkedin@gmail.com>
2026-01-23 20:47:29 +0530

    new code

---
+++ Code/4.Array/sellStock.java
@@ -0,0 +1,21 @@
+import java.util.*;
+public class sellStock {
+    public static void stockSell(int prices[]){
+        int buyPrice = Integer.MAX_VALUE;
+        int maxProfit=0;
+        for(int i=0;i<prices.length;i++){
+            if(buyPrice<prices[i]){
+                int profit = prices[i]-buyPrice;
+                maxProfit = Math.max(maxProfit,profit);
+            }else{
+                buyPrice = prices[i];
+            }
+        }
+        System.out.println("Maximum Profit is: "+maxProfit);
+
+    }
+    public static void main(String args[]){
+        int prices[] = {7,1,5,3,6,4};
+        stockSell(prices);
+    }
+}
